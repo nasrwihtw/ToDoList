857ebb19fc1d787e8fe37fe9cfd51373cf73d13a
Osamah Al-Awadhi <Osamah.Al-Awadhi@Student.HTW-Berlin.de>
2023-10-30 21:35:56 +0100

    database configurtation

--- src/main/java/de/htwberlin/ToDoList/Controller/HelloWorldController.java
+++ src/main/java/de/htwberlin/ToDoList/Controller/HelloWorldController.java
@@ -2,12 +2,11 @@ package de.htwberlin.ToDoList.Controller;
 
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sun.tools.attach.AgentInitializationException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
-
     @GetMapping("/")
     public String HelloWorld() {
         return "Hello World!";
